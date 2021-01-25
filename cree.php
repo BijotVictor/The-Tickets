635781195daf3c5dbfe8f6bc04be48503903711a
BijotVictor <70894571+BijotVictor@users.noreply.github.com>
2021-01-25 15:19:10 +0100

    up

--- cree.php
+++ cree.php
@@ -1,17 +1,63 @@
-<!DOCTYPE html>
-<html>
+<?php
+// On démarre une session
+session_start();
 
+if($_POST){
+    if(isset($_POST['inputEmail4']) && !empty($_POST['inputEmail4'])
+    && isset($_POST['inputPassword4']) && !empty($_POST['inputPassword4'])
+    && isset($_POST['inputAddress']) && !empty($_POST['inputAddress'])
+    && isset($_POST['inputCity']) && !empty($_POST['inputCity'])
+    && isset($_POST['inputState']) && !empty($_POST['inputState'])
+    && isset($_POST['inputZip']) && !empty($_POST['inputZip'])){
+        // On inclut la connexion à la base
+        require_once('connect.php');
+
+        // On nettoie les données envoyées
+        $email = strip_tags($_POST['inputEmail4']);
+        $password = strip_tags($_POST['inputPassword4']);
+        $address = strip_tags($_POST['inputAddress']);
+        $city = strip_tags($_POST['inputCity']);
+        $state = strip_tags($_POST['inputState']);
+        $zip= strip_tags($_POST['inputZip']);
+
+        $sql = 'INSERT INTO `evenement` (`email`, `password`, `address`,`city`,`state`,`zip`) VALUES (:email, :password, :address , :city , :state , :zip);';
+
+        $query = $db->prepare($sql);
+
+        $query->bindValue(':email', $email, PDO::PARAM_STR);
+        $query->bindValue(':password', $password, PDO::PARAM_STR);
+        $query->bindValue(':address', $address, PDO::PARAM_STR);
+        $query->bindValue(':city', $city, PDO::PARAM_STR);
+        $query->bindValue(':state', $state, PDO::PARAM_STR);
+        $query->bindValue(':zip', $zip, PDO::PARAM_STR);
+
+        $query->execute();
+
+        $_SESSION['message'] = "Evenement ajouté";
+        require_once('close.php');
+
+        header('Location: accueil.php');
+    }else{
+        $_SESSION['erreur'] = "Le formulaire est incomplet";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+ <!-- Required meta tags -->
+ <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>The Tickets</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="style4.css">
-</head>
+    <title>Creer un evenement</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
 <body>
 <nav class="navbar navbar-dark bg-dark position ">
         <a class="navbar-brand " href="accueil.php">
@@ -28,28 +74,38 @@
             <a href="login.php"><button id="login"> Se connecter </button></a>
         </ul>
     </nav>
-    <br><br>
-    <form class="row g-3 container mx-auto">
+    <main class="container">
+        <div class="row">
+                <?php
+                    if(!empty($_SESSION['erreur'])){
+                        echo '<div class="alert alert-danger" role="alert">
+                                '. $_SESSION['erreur'].'
+                            </div>';
+                        $_SESSION['erreur'] = "";
+                    }
+                ?>
+                <h1>Ajouter un Evenement</h1>
+                  <form method="post" class="row g-3 container mx-auto">
         <div class="col-md-6">
             <label for="inputEmail4" class="form-label">Email de contact</label>
-            <input type="email" class="form-control" id="inputEmail4">
+            <input type="email" class="form-control" id="inputEmail4" name="inputEmail4">
         </div>
         <div class="col-md-6">
             <label for="inputPassword4" class="form-label">Code secret de l'évenement</label>
-            <input type="password" class="form-control" id="inputPassword4">
+            <input type="password" class="form-control" id="inputPassword4" name="inputPassword4">
         </div>
         <div class="col-12">
             <label for="inputAddress" class="form-label">Adresse de l'évenement</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="1234 Main St">
         </div>
 
         <div class="col-md-6">
             <label for="inputCity" class="form-label">Ville de l'événement</label>
-            <input type="text" class="form-control" id="inputCity">
+            <input type="text" class="form-control" id="inputCity" name="inputCity">
         </div>
         <div class="col-md-4">
             <label for="inputState" class="form-label">Departement de l'évenement</label>
-            <select id="inputState" class="form-select">
+            <select id="inputState" name="inputState" class="form-select">
             <option selected>Choosis...</option>
             <option>01 - Ain - Bourg-en-bresse</option>
             <option>02 - Aisne - Laon</option>
@@ -61,44 +117,14 @@
         </div>
         <div class="col-md-2">
             <label for="inputZip" class="form-label">Code postal</label>
-            <input type="text" class="form-control" id="inputZip">
+            <input type="text" class="form-control" id="inputZip" name="inputZip">
         </div>
         <div class="col-12">
             <a class="btn btn-primary" href="creationterminer.html" role="button">Créer l'évenement</a>
         </div>
 
     </form>
-
-    <footer class="position fixed-bottom container py-3 bg-light">
-        <div class="row">
-            <div class="col-12 col-md">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-                <small class="d-block mb-3 text-muted">&copy; equipe dev web The Tickets : 2020-2021</small>
-                <p>L'equipe de dévelopeur</p>
-            </div>
-            <div class="col-6 col-md">
-                <br>
-                <h5>DEV </h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Alice Petitjean</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <br>
-                <h5>DEV</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Leandre Gaden</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <br>
-                <h5>DEV</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Victor Bijot</a></li>
-                </ul>
-            </div>
         </div>
-    </footer>
+    </main>
 </body>
-
 </html>
